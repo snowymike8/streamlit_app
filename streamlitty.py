0fc390f8ce663c36e1079a99d498063336dfc2fe
Michael T <122465815+snowymike8@users.noreply.github.com>
2023-01-19 10:42:35 -0500

    Update streamlitty.py

--- streamlitty.py
+++ streamlitty.py
@@ -13,7 +13,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 #Fruit Picker
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 #Table of options
 streamlit.dataframe(my_fruit_list)
 
