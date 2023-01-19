355d88d6bb93756deadd79a0eb7926a1e11da3c4
Michael T <122465815+snowymike8@users.noreply.github.com>
2023-01-19 10:46:37 -0500

    Update streamlitty.py

--- streamlitty.py
+++ streamlitty.py
@@ -14,9 +14,9 @@ import pandas
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 #Fruit Picker
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+selected_fruits = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))#,['Avocado','Strawberries'])
 #Table of options
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list[selected_fruits])
 
 
 
