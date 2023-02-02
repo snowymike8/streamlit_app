d8625cc1786b5e1f200fa28c2783455693c65764
Michael T <122465815+snowymike8@users.noreply.github.com>
2023-02-02 16:24:13 -0500

    Update streamlitty.py

--- streamlitty.py
+++ streamlitty.py
@@ -42,18 +42,24 @@ except URLError as e:
 
 
 
-streamlit.stop()
+#streamlit.stop()
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+#Snowflake-realted functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+  
+#Add button to load fruit:
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_row = get_fruit_load_list()
+  streamlit.dataframe(my_data_row)
 #streamlit.text("The fruit load list contains:")
 #streamlit.text(my_data_row)
-added_fruit = streamlit.text_input("What fruit would you like to add?","jackfruit")
-streamlit.write('Thanks for adding ', added_fruit)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#added_fruit = streamlit.text_input("What fruit would you like to add?","jackfruit")
+#streamlit.write('Thanks for adding ', added_fruit)
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
